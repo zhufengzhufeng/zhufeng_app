86b2ec9c59c74d5d0eb418b171b526d63f08488a
pamzer156 <1035465285@qq.com>
2017-05-30 16:37:58 +0800

    connect

--- app/containers/Home/index.js
+++ app/containers/Home/index.js
@@ -2,14 +2,21 @@ import React,{Component} from 'react';
 import HomeHeader from "../../components/HomeHeader/index";
 import Slider from "../../components/Slider/index";
 import Ad from "./subpage/Ad";
-export default class Home extends Component{
+import {connect} from 'react-redux';
+class Home extends Component{
     render(){
         return (
             <div>
-                <HomeHeader cityName="北京"/>
+                <HomeHeader cityName={this.props.userInfo.cityName}/>
                 <Slider/>
                 <Ad/>
             </div>
         )
     }
 }
+//取出redux中的城市传递给HomeHeader组件
+export default connect(
+    state=>{
+        return {userInfo:state.userInfo} //取出redux state中userInfo的数据，赋予给变量userInfo
+    }
+)(Home);
--- app/containers/index.js
+++ app/containers/index.js
@@ -4,7 +4,8 @@ import {connect} from 'react-redux';
 //绑定actionCreator组成的对象
 import {bindActionCreators} from 'redux';
 //获取所有actionCreator组成的对象
-import * as Actions from '../actions/userInfo'
+import * as Actions from '../actions/userInfo';
+import {getStorage} from '../local'
 class App extends Component{
     constructor(){
         super();
@@ -24,20 +25,27 @@ class App extends Component{
         //先去本地查找 是否存储过localstorage名字叫cityName的
         //1.没有cityName ，赋予一个默认值 杭州
         //2.将当前的城市存放到redux
-
-
+        let cityName = getStorage('cityName');
+        if(cityName == null){
+            cityName = '杭州'
+        }
+        //页面加载后，设置一个城市
+        this.props.userActions.update({
+            cityName
+        });
         this.setState({
             done:true
         });
-        console.log(this.props.userActions);
+
     }
 }
 export default connect(
-    state=>{ // mapStateToProps
+    state=>{
         return {}
     },
     dispatch=>{ //mapDispatchToProps
         return {
+            //dispatch(action,data);
             userActions:bindActionCreators(Actions,dispatch)
         }
     }
