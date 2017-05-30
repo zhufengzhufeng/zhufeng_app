c722746f27c6563c13332e973c5ef2bd199cceee
pamzer156 <1035465285@qq.com>
2017-05-30 16:01:38 +0800

    connect

--- app/containers/index.js
+++ app/containers/index.js
@@ -1,6 +1,11 @@
 import React,{Component} from 'react';
 import RouterMap from "../routes/index";
-export default class App extends Component{
+import {connect} from 'react-redux';
+//绑定actionCreator组成的对象
+import {bindActionCreators} from 'redux';
+//获取所有actionCreator组成的对象
+import * as Actions from '../actions/userInfo'
+class App extends Component{
     constructor(){
         super();
         this.state = {
@@ -16,8 +21,24 @@ export default class App extends Component{
         )
     }
     componentDidMount(){
+        //先去本地查找 是否存储过localstorage名字叫cityName的
+        //1.没有cityName ，赋予一个默认值 杭州
+        //2.将当前的城市存放到redux
+
+
         this.setState({
             done:true
         });
+        console.log(this.props.userActions);
     }
 }
+export default connect(
+    state=>{ // mapStateToProps
+        return {}
+    },
+    dispatch=>{ //mapDispatchToProps
+        return {
+            userActions:bindActionCreators(Actions,dispatch)
+        }
+    }
+)(App)
