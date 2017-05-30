c50984a64c692625c94c89e0e68e63604977283f
pamzer156 <1035465285@qq.com>
2017-05-30 12:58:26 +0800

    ad页

--- app/containers/Home/subpage/Ad.js
+++ app/containers/Home/subpage/Ad.js
@@ -1,5 +1,6 @@
 import React,{Component} from 'react';
-import {getAd} from '../../../fetch/home'
+import {getAd} from '../../../fetch/home';
+import './index.less'
 export default class Ad extends Component{
     constructor(){
         super();
@@ -9,13 +10,21 @@ export default class Ad extends Component{
     }
     render(){
         return (
-            <div>
+            <div className="ad">
+                <h3>超值特惠</h3>
+                {this.state.data.length?
+                    this.state.data.map((item,index)=>(
+                        <a href={item.link} key={index}>
+                            <img src={item.img} title={item.title}/>
+                        </a>
+                    )):
+                    <div>正在加载</div>
+                }
             </div>
         )
     }
     componentDidMount(){
         getAd().then(res=>res.json()).then(data=>{
-            console.log(data);
             this.setState({
                 data
             })
