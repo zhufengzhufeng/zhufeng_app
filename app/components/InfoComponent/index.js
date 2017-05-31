e4b4f987031f43b3855158815c0c37318f3e322b
pamzer156 <1035465285@qq.com>
2017-05-31 13:00:39 +0800

    infoComponent

--- app/components/InfoComponent/index.js
+++ app/components/InfoComponent/index.js
@@ -1,21 +1,24 @@
 import React,{Component} from 'react';
 import Star from "../Star/index";
+import './index.less'
 export default class InfoComponent extends Component{
     render(){
         let {img,star,desc,title,subTitle,price} = this.props.data;
         return (
-            <div>
-                <div>
+            <div className="info-component">
+                <div className="info-list">
                     <img src={img} alt=""/>
                     <div>
                         <h3>{title}</h3>
-                        <div>
+                        <div className="info-star">
                             {/*star组件要接收一个点亮的星星数量*/}
                             <Star count={star}/> <span>￥{price}</span>
                         </div>
                         <p>{subTitle}</p>
                     </div>
                 </div>
+                <br/>
+                <hr/>
                 {/*将内容转换成html插入到页面中*/}
                 <div dangerouslySetInnerHTML={{__html:desc}}></div>
             </div>
