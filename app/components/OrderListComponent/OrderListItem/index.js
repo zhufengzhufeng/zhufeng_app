ad5ccfb366513ff6cc72428e3817684cfc31b13e
pamzer156 <1035465285@qq.com>
2017-06-01 10:28:00 +0800

    订单详情页

--- app/components/OrderListComponent/OrderListItem/index.js
+++ app/components/OrderListComponent/OrderListItem/index.js
@@ -1,16 +1,17 @@
 import React,{Component} from 'react';
+import './index.less'
 export default class OrderListItem extends Component{
     render(){
         let {id,commentState,img,title,count,price} = this.props.data;
         return (
-            <div>
+            <div className="order-list-item">
                 <img src={img} alt=""/>
-                <div>
+                <div className="list-content">
                     <h3>商户:{title}</h3>
                     <span>数量:{count}</span>
                     <span>价格:{price}</span>
                 </div>
-                <div>
+                <div className="order-btn">
                     <button>评价</button>
                 </div>
             </div>
